728eea180641660a671a8c03b537a8b2500a7412
LucasPierreAraujo <pedrolucaspierre@gmail.com>
2025-03-18 10:17:55 -0300

    att

--- src/app/cardapio/page.jsx
+++ src/app/cardapio/page.jsx
@@ -27,12 +27,22 @@ export default function Home() {
       </header>
 
         <div className="flex flex-wrap items-center justify-center h-screen gap-2">
-          
+
             <Image  alt="home" src={"/cardapio/home.jpeg"} width={600} height={600} />
 
             <Image alt="home1" src={"/cardapio/home1.jpeg"} width={600} height={600} />
 
             <Image alt="home2" src={"/cardapio/home2.jpeg"} width={600} height={600} />
+
+            <Image alt="formulario1" src={"/cardapio/formulario1.jpeg"} width={600} height={600} />
+
+            <Image alt="formulario2" src={"/cardapio/formulario2.jpeg"} width={600} height={600} />
+
+            <Image alt="formulario3" src={"/cardapio/formulario3.jpeg"} width={600} height={600} />
+
+            <Image alt="formulario4" src={"/cardapio/formulario4.jpeg"} width={600} height={600} />
+
+            <Image alt="formulario5" src={"/cardapio/formulario5.jpeg"} width={600} height={600} />
             
         </div>
         
